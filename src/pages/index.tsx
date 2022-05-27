fa4177c51a8c1eb674573119f91d5203886f5475
Andrew Nosov <andrewnosov@outlook.com>
2022-05-27 11:15:22 +0500

    TEST cart context

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,46 @@
-import { LoadingDots } from "@/components/atoms/loading";
+import { GetStaticProps } from 'next';
 
-const Home = () => {
+import { products } from '@/data/products';
+import { Product } from '@/types/product';
+import { useCartContext } from '@/contexts/cart';
+
+export const getStaticProps: GetStaticProps = () => {
+    return {
+        props: {
+            products,
+        },
+    };
+};
+
+type HomeProps = {
+    products: Product[];
+};
+
+const Home = ({ products }: HomeProps) => {
+    const { addProductToCart } = useCartContext();
+
     return (
         <>
-            <h1>Привет мир моды!</h1>
+            <h1>Kuku</h1>
+            <ul>
+                {products.map((p) => (
+                    <li key={p.id}>
+                        <p>{p.title}</p>
+                        <button
+                            onClick={() =>
+                                addProductToCart({
+                                    ...p,
+                                    quantity: 1,
+                                })
+                            }
+                        >
+                            Add to cart
+                        </button>
+                    </li>
+                ))}
+            </ul>
         </>
     );
-}
+};
 
 export default Home;
