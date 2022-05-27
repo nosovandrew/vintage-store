33075033720c30fbee1e9c896ca9a4862568d662
Andrew Nosov <andrewnosov@outlook.com>
2022-05-27 11:15:05 +0500

    ADD mock data

---
+++ src/data/products.ts
@@ -0,0 +1,13 @@
+// temporary data
+export const products = [
+    {
+        id: 1,
+        title: 'Canvas Bag',
+        price: 5000,
+    },
+    {
+        id: 2,
+        title: 'Puffer Bag',
+        price: 10000,
+    }
+];
